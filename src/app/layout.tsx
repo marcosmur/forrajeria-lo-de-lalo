c7cf530fef9713b0fb1f7907fb777a174872e2c6
marcos murphy <marcosmurphy11@gmail.com>
2025-09-28 23:15:55 -0300

    arreglo de errores y productos completos

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import { CartProvider } from "@/context/CartContext";
 import Navbar from "@/components/Navbar";
 import Carrito from "@/components/Carrito";
-import Footer from "@/components/Footer"; // 👈 importar Footer
+import Footer from "@/components/footer";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -17,14 +17,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="es">
-      <body>
-        <CartProvider>
-          <Navbar />
-          <Carrito />
-          <main className="min-h-screen">{children}</main>
-          <Footer /> {/* 👈 acá se renderiza el footer en todas las páginas */}
-        </CartProvider>
-      </body>
+      <body className="amp-mask">
+  <CartProvider>
+    <Navbar />
+    <Carrito />
+    <main className="min-h-screen">{children}</main>
+    <Footer />
+  </CartProvider>
+</body>
     </html>
   );
 }
